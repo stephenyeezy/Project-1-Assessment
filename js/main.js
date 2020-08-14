c26d6d600fa7ba564e005f35c4f11b4bd7514570
stephen yi <stephenyi@stephens-MacBook-Pro.local>
2020-08-14 17:28:37 -0400

    assessment

---
+++ js/main.js
@@ -0,0 +1,46 @@
+// variables
+let inputValue;
+let displayTotal;
+
+// element reference
+numberEl = document.querySelector("input");
+runTotalEl = document.querySelector('h3')
+// event listeners
+document.getElementById("plus").addEventListener('click', addTotal)
+document.getElementById("minus").addEventListener('click', subTotal)
+// functions
+
+let i = parseInt(numberEl.value)
+
+init();
+function init() {
+    displayTotal = 0;
+    runTotalEl.textContent = displayTotal;
+    render();
+}
+
+function render() {
+    if (displayTotal < 0) {
+        runTotalEl.textContent = displayTotal;
+        runTotalEl.style.color = 'red'
+    } else {
+        runTotalEl.textContent = displayTotal;
+        runTotalEl.style.color = 'black'
+    }
+    
+}
+
+function addTotal(evt) {
+    i = parseInt(numberEl.value)
+    numberEl.textContent = i;
+    displayTotal += i;
+    render();
+    
+}
+function subTotal(evt) {
+    i = parseInt(numberEl.value)
+    numberEl.textContent = i;
+    displayTotal -= i;
+    render();
+    
+}
